27876068aeac441c4b6bcfeb4f22601ca47d03f6
ridshere <rithamdon@gmail.com>
2026-06-12 19:14:11 +0530

    Add electricity bill calculation program

---
+++ electricity_bill.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill;
+
+    // User se units input lena
+    printf("Electricity Bill Generator\n");
+    printf("Enter electricity units consumed: ");
+    scanf("%d", &units);
+
+    // Bill calculation
+    if (units <= 100) {
+        bill = units * 1.5;
+    }
+    else if (units <= 200) {
+        bill = (100 * 1.5) + ((units - 100) * 2.5);
+    }
+    else {
+        bill = (100 * 1.5) + (100 * 2.5) + ((units - 200) * 4.0);
+    }
+
+    // Output
+    printf("Total Electricity Bill = Rs %.2f\n", bill);
+
+    return 0;
+}
